dfef38adb9374b11ceaf7183396f56a1fff7f832
declercklouis <louis.declerck@student.howest.be>
2023-12-07 18:38:13 +0100

    strat_controller

---
+++ 02_tests/unifi/start_controller.sh
@@ -0,0 +1,1 @@
+docker run -d --init    --restart=unless-stopped    -p 8080:8080 -p 8443:8443 -p 3478:3478/udp    -e TZ='Europe/Brussels'    -v ~/unifi:/unifi    --user unifi    --name unifi    jacobalberty/unifi
